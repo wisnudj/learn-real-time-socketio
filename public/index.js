96d663bbe6ddc1cd149c838c0584009fc8ffcade
wisnu <wisnu@mahapatih.co.id>
2018-08-07 15:38:43 +0700

    form-jquery

--- public/index.js
+++ public/index.js
@@ -12,11 +12,27 @@ socket.on('connect', function() {
 })
 
 // received email from server that emit newEmail
-socket.on('newMessage', function(email) {
-    console.log('new Message', email)
+socket.on('newMessage', function(message) {
+    console.log('new Message', message)
+
+    var li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+
+    jQuery("#messages").append(li)
 })
 
 // Received this message if disconnect from server
 socket.on('disconnect', function() {
     console.log('disconnected from server')
+})
+
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault()
+    socket.emit("createMessage", {
+        from: "jquery client",
+        text: jQuery('[name=message]').val()
+    }, function(data) {
+        console.log('Got it ', data)
+    })
 })
